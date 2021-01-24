77b9c970628bfbe1a66bcbe5a0ce050e1bb656d7
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-01-24 18:37:43 +0300

    Слово заканчивается с ... [#230945]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = false;
         for (int i = 0; i < pref.length; i++) {
-            if (pref[i] == word[i]) {
+            if (pref[i] ==  word[i]) {
                 result = true;
             } else {
                 result = false;
