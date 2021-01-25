0edbb41d136070547a997213cdc63b79659bfc62
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-01-26 00:21:12 +0300

    Сортировка выборкой. [#230947] - Refactor

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -14,4 +14,20 @@ public class SortSelectedTest {
         int[] expected = {1, 2, 3, 4, 5};
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void whenSort1() {
+        int[] input = {3, 1, 2};
+        int[] result = SortSelected.sort(input);
+        int[] expect = {1, 2, 3};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort2() {
+        int[] input = {2, 4, 1, 5, 6, 10, 7, 9, 8, 3};
+        int[] result = SortSelected.sort(input);
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(result, is(expected));
+    }
 }
