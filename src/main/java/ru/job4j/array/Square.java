24008e02a7861392942d956857bd2ec4691328de
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-01-28 22:51:59 +0300

    Test

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -1,8 +1,7 @@
 package ru.job4j.array;
 
-import ru.job4j.Calculator;
-
 public class Square {
+
     public static int[] calculate(int bound) {
         int[] rs1 = new int[bound];
         for (int i = 0; i < rs1.length; i++) {
@@ -10,6 +9,7 @@ public class Square {
         }
         return rs1;
     }
+
     public static void main(String[] args) {
         int[] result = Square.calculate(4);
         for (int i = 0; i < result.length; i++) {
