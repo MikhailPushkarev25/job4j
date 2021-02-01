e8b8037d3846e8355bc72953b26b8db3caeec49e
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-02-01 19:52:40 +0300

    1.5. Поля объекта [#231105]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -2,9 +2,35 @@ package ru.job4j.oop;
 
 /**
  * Class Cat - В этом классе я добавил метод с возвращаемым типом
- * и показываю как работает этот метод
+ * и показываю как работает этот метод, Так же поля.
  */
 public class Cat {
+
+    /**
+     * Поле
+     */
+    private String food;
+    private String name;
+
+    /**
+     * Вывод поля
+     */
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    /**
+     *
+     * @param meat - запись строки в поле.
+     */
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
 
     /**
      * @ sound - метод с возвращаемым типом.
@@ -23,5 +49,16 @@ public class Cat {
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        System.out.println();
+        System.out.println("There are gav food");
+        Cat gav = new Cat();
+        gav.giveNick("Pushok");
+        gav.eat("Kotleta");
+        gav.show();
+        System.out.println("There are black food");
+        Cat black = new Cat();
+        black.giveNick("Big");
+        black.eat("Fish");
+        black.show();
     }
 }
