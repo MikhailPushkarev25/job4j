c64cdba121392baa6d0acaf15214becf7e17778e
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-02-14 15:13:30 +0300

    17. Транспонировать матрицу

---
+++ chapter_002/src/main/java/ru/job4j/array/Transposition.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * @author Mikhail Pushkarev
+ * @since 14.02.2021
+ * @version 2.2
+ *
+ * В это классе я транспортировал массив строк в столбцы
+ */
+public class Transposition {
+
+    public static int[][] convert(int[][]matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+}
