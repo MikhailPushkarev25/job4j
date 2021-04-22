0f15cb3656f2b446409e0a9c3242284f9d97d5e7
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-04-22 22:15:14 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy. [#296927]

--- chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -6,14 +6,15 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 
 public class SqlTrackerTest {
 
-    @Test
     public Connection init() {
         try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
             Properties config = new Properties();
@@ -36,5 +37,58 @@ public class SqlTrackerTest {
             assertThat(tracker.findByName("name").size(), is(1));
         }
 
+    }
+
+    @Test
+    public void replace() throws SQLException {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+           Item item = new Item("name");
+            Item items = new Item("names");
+            tracker.add(item);
+            tracker.replace(item.getId(), items);
+            assertThat(tracker.findByName("names").size(), is(1));
+
+        }
+    }
+
+    @Test
+    public void delete() throws SQLException {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item("item");
+            tracker.add(item);
+            tracker.delete(item.getId());
+            assertNull(tracker.findById(item.getId()));
+        }
+    }
+
+    @Test
+    public void findAll() throws SQLException {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+          Item item = new Item("name");
+          Item item1 = new Item("names");
+          tracker.add(item);
+          tracker.add(item1);
+           assertThat(tracker.findAll().size(), is(8));
+        }
+    }
+
+    @Test
+    public void findByName() throws SQLException {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item("name");
+            tracker.add(item);
+            List<Item> list = tracker.findByName(item.getName());
+            assertThat(list.get(0).getName(), is("name"));
+        }
+    }
+
+    @Test
+    public void findById() throws SQLException {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item item = new Item("name");
+            tracker.add(item);
+            int id = item.getId();
+            assertThat(tracker.findById(id).getName(), is(item.getName()));
+        }
     }
 }
