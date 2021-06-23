9a8ed6ee26ba766135ce3690e09aefc1eec42d1d
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-06-23 22:08:22 +0300

    2.5. Ленивая загрузка [#249211]

---
+++ chapter_004/src/main/java/function/Excepted.java
@@ -0,0 +1,47 @@
+package function;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author Mikhail Pushkarev
+ * @since 23.06.2021
+ * @version 2.2
+ *
+ * В классе демонстрирую продуктивную работу лямбда
+ */
+
+public class Excepted {
+    public static void main(String[] args) {
+        /**
+         * В первом случае я использовал массив с одним элементом
+         * далее с помощью компаратора прохожу по убыванию
+         * так как элемент один, вывода не будет
+         */
+        String[] names = {
+                "Ivan"
+        };
+
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return left.length() - right.length();
+        };
+        Arrays.sort(names, lengthCmp);
+
+        String[] name = {
+                "Ivan",
+                "Roman"
+        };
+
+        /**
+         * Во втором случает все тоже самое,
+         * но уже два элемента и программа сработает
+         * так как компаратору есть что делать с элементами
+         */
+
+        Comparator<String> length = (left, right) -> {
+            System.out.println("execute comparator");
+            return left.length() - right.length();
+        };
+        Arrays.sort(name, length);
+    }
+}
