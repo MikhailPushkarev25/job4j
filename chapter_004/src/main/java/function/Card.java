97bb76c128c27d3dcd3c2c1649d315bbf99cc18d
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-06-25 20:24:16 +0300

    8. Перенос методов в Stream API [#279131 #170916]

--- chapter_004/src/main/java/function/Card.java
+++ chapter_004/src/main/java/function/Card.java
@@ -15,10 +15,20 @@ enum Value {
      private Suit suit;
      private Value value;
 
+     public Card(Suit suit, Value value) {
+         this.suit = suit;
+         this.value = value;
+     }
+
+     @Override
+     public String toString() {
+         return "Card{" + "suit=" + suit + ", value=" + value + '}';
+     }
+
      public static void main(String[] args) {
          Stream.of(Suit.values())
                  .flatMap(suit -> Stream.of(Value.values())
-                 .map(v -> suit + " " + v))
+                 .map(v -> new Card(suit, v)))
                          .forEach(System.out::println);
      }
  }
